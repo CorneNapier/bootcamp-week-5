21c0a4d93dd774c719963a0384f9017b0061658a
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-22 11:11:23 +0000

    Update script.js
    Added some notes about objects and how to access a property of an object

--- Day1/script.js
+++ Day1/script.js
@@ -10,4 +10,10 @@ let library = {
     harryPotterPhilospherStone: {author: "", ISBN:32075325025, location: "R985", genre: ""},
     harryPotterPhilospherStone: {author: "", ISBN:32075325025, location: "R985", genre: ""},
     harryPotterPhilospherStone: {author: "", ISBN:32075325025, location: "R985", genre: ""},
-}
+};
+
+// {} defines we using an object
+// (dot) . notation allows us to access a property of an object
+
+//accessing property in library object
+console.log(harryPotterPhilospherStone.harryPotterPhilospherStone.ISBN);
