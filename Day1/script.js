c0468e100dc7cd2ee482565b61cbd804e894850d
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-22 09:35:19 +0000

    Created day1 folder
    Added html and js files. In my script.js added example of objects.

---
+++ Day1/script.js
@@ -0,0 +1,13 @@
+let person = {
+    name: 'Corné',
+    age: 30
+};
+
+//example of an object using other objects
+let library = {
+    harryPotterPhilospherStone: {author: "", ISBN:32075325025, location: "R985", genre: ""},
+    harryPotterPhilospherStone: {author: "", ISBN:32075325025, location: "R985", genre: ""},
+    harryPotterPhilospherStone: {author: "", ISBN:32075325025, location: "R985", genre: ""},
+    harryPotterPhilospherStone: {author: "", ISBN:32075325025, location: "R985", genre: ""},
+    harryPotterPhilospherStone: {author: "", ISBN:32075325025, location: "R985", genre: ""},
+}
