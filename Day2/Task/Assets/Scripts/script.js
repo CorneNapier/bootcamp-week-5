92a767ac46ae5ede954227922b1fea85a87537f9
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-23 17:32:11 +0000

    Updated script.js
    Added completed tasks

--- Day2/Task/Assets/Scripts/script.js
+++ Day2/Task/Assets/Scripts/script.js
@@ -1,35 +1,56 @@
+//Task1
 let food = document.getElementById('food');
 let drink = document.getElementById('drinks');
 let btn = document.getElementById('btn');
 
-btn.addEventListener("click", function run(){
-    if (food.style.display == "block") {
-        food.style.display = "none"
-        drink.style.display = "block"
-        btn.innerText = "Food Menu"
-    } else {
+btn.addEventListener("click", function run() {
+    if (drink.style.display == "block") {
         food.style.display = "block"
         drink.style.display = "none"
         btn.innerText = "Drink Menu"
+    } else {
+        food.style.display = "none"
+        drink.style.display = "block"
+        btn.innerText = "Food Menu"
     }
 })
-
+//Task2
 let links = document.querySelectorAll('nav a');
 
-for (let link in links) {
-    links[link].addEventListener(
-        "mouseover",
-        (event) => {
-          // highlight the mouseover target
-          event.target.style.color = "orange";
-          event.target.style.fontFamily = "Imapact";
-      
-          // reset the color after a short delay
-          setTimeout(() => {
-            event.target.style.color = "";
-            event.target.style.fontFamily = "";
-          }, 500);
-        },
-        false,
-      );
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("mouseover", function run() {
+        links[i].style.color = "orange";
+        links[i].style.fontFamily = "Impact";
+        links[i].style.fontSize = "x-large";
+
+    })
+
+    links[i].addEventListener("mouseout", function run() {
+        links[i].style.color = "";
+        links[i].style.fontFamily = "";
+        links[i].style.fontSize = "";
+
+    })
+}
+//Task3
+let openings = document.getElementById('openings');
+
+openings.addEventListener('mouseenter', function run() {
+    openings.style.fontSize = "x-large";
+})
+
+openings.addEventListener('mouseleave', function run() {
+    openings.style.fontSize = "";
+})
+//Task4
+let myInput = document.getElementsByTagName('input')
+
+for (let i = 0; i < myInput.length; i++) {
+    const input = myInput[i];
+
+    input.addEventListener('keypress', function run() {
+        input.style.backgroundColor = "blue";
+        input.style.color = "red";
+    })
+
 }
